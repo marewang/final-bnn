a5fe4f4687a0ba1e54acd9dadd9b04ee8b5ba83c
marewang <113419989+marewang@users.noreply.github.com>
2025-08-27 19:57:41 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: false },
-  eslint: { ignoreDuringBuilds: true }
+// next.config.mjs
+const csp = `
+default-src 'self';
+connect-src 'self' https://*.vercel-insights.com;
+img-src 'self' data: blob:;
+script-src 'self' 'unsafe-inline' 'unsafe-eval' vercel.live;
+style-src 'self' 'unsafe-inline';
+font-src 'self' data:;
+frame-ancestors 'self';
+form-action 'self';
+`.replace(/\n/g, ' ');
+
+export default {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Content-Security-Policy', value: csp },
+        ],
+      },
+    ];
+  },
 };
-export default nextConfig;
+
